641383e495d15c6e913bed6418ae88fbd19447b2
Jeeyoung Choi <jeeyoungchoi@Jessicas-MacBook-Air.local>
2021-11-04 23:19:19 -0400

    reading data

--- data_reading.R
+++ data_reading.R
@@ -1,6 +1,7 @@
 library(readr)
+library(tidyverse)
 
-us_cancer <- read_delim("us_cancer.txt", 
+nl_cancer <- read_delim("nervous_lung_cancer.txt", 
                         delim = "\t", escape_double = FALSE, 
                         trim_ws = TRUE)
 
@@ -11,3 +12,14 @@ leading_cancer <- read_delim("leading_cancer.txt",
                              delim = "\t", escape_double = FALSE, 
                              trim_ws = TRUE)
 
+leading_death <- read_delim("leading_death.txt", 
+                            delim = "\t", escape_double = FALSE, 
+                            trim_ws = TRUE)
+
+child_death <- read_delim("leading_child_death.txt", 
+                            delim = "\t", escape_double = FALSE, 
+                            trim_ws = TRUE)
+
+nl_death <- read_delim("nl_death.txt", 
+                          delim = "\t", escape_double = FALSE, 
+                          trim_ws = TRUE)
