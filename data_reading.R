27dcef47b7130566bbccfe071b53b3c2148e1bc4
Jeeyoung Choi <jeeyoungchoi@Jessicas-MacBook-Air.local>
2021-11-10 21:39:30 -0500

    Added visualization file

--- data_reading.R
+++ data_reading.R
@@ -1,6 +1,15 @@
-library(readr)
 library(tidyverse)
 
+#clean text file 
+clean_text <- function(file){
+  file <- file[,-1] #remove the "notes" column, which is NA values 
+  #some rows are completely empty, filter them out 
+  file <- file[which(complete.cases(file)),]
+  file <- within(file, rm("Year Code"))
+  return(file)
+}
+
+#read in the text files 
 nl_cancer <- read_delim("nervous_lung_cancer.txt", 
                         delim = "\t", escape_double = FALSE, 
                         trim_ws = TRUE)
@@ -8,18 +17,35 @@ nl_cancer <- read_delim("nervous_lung_cancer.txt",
 child_cancer <- read_delim("child_cancer.txt", 
                            delim = "\t", escape_double = FALSE, 
                            trim_ws = TRUE)
+
 leading_cancer <- read_delim("leading_cancer.txt", 
                              delim = "\t", escape_double = FALSE, 
                              trim_ws = TRUE)
 
-leading_death <- read_delim("leading_death.txt", 
-                            delim = "\t", escape_double = FALSE, 
-                            trim_ws = TRUE)
+nl_death <- read_delim("nl_death.txt", 
+                       delim = "\t", escape_double = FALSE, 
+                       trim_ws = TRUE)
 
 child_death <- read_delim("leading_child_death.txt", 
                             delim = "\t", escape_double = FALSE, 
                             trim_ws = TRUE)
 
-nl_death <- read_delim("nl_death.txt", 
-                          delim = "\t", escape_double = FALSE, 
-                          trim_ws = TRUE)
+
+leading_death <- read_delim("leading_death.txt", 
+                            delim = "\t", escape_double = FALSE, 
+                            trim_ws = TRUE)
+
+options(warn=-1)
+#clean data frames
+nl_cancer <- clean_text(nl_cancer)
+child_cancer <- clean_text(child_cancer)
+leading_cancer <- clean_text(leading_cancer)
+nl_death <- clean_text(nl_death)
+child_death <- clean_text(child_death)
+leading_death <- clean_text(leading_death)
+
+options(warn=0)
+
+#obtain only CNS cancers
+df <- child_cancer[grepl("III(b) Astrocytomas", child_cancer$`Childhood Cancers`,fixed = TRUE),]
+
---
+++ data_visualization.R
@@ -0,0 +1,42 @@
+source("data_reading.R")
+pacman::p_load(knitr,
+        arm,
+        data.table,
+        foreign,
+        gridExtra,
+        rstan,
+        rstanarm,
+        zoo,
+        lme4, 
+        magrittr,
+        maps,
+        shiny)
+
+year <- df %>% distinct(Year)
+year <- year$Year
+MainStates <- map_data("state")
+MainStates$region <- str_to_title(MainStates$region)
+colnames(MainStates) <- c("long", "lat", "group", "order", "States", "subregion")
+
+
+ui_2 <- fluidPage(
+  selectInput("Year", "select year", year), 
+  plotOutput("plot")
+)
+server_2 <- function(input, output, session) {
+  output$plot <- renderPlot({
+    #df_mean <- df %>% subset(Year == input$Year) %>% group_by(States) %>% summarise(mean_value = mean(Value))
+    plot_data <- inner_join(df, MainStates, by = "States") %>% 
+      dplyr::select("States", "long", "lat", "Age-Adjusted Rate", "Year")
+    ggplot() + 
+      geom_polygon(data=MainStates, aes(x=long, y=lat, group=group),color="black", fill="seashell1", size = .3) + 
+      geom_polygon(data = plot_data, aes(x = long, y = lat, group = States, fill = "Age-Adjusted Rate"), 
+                   color = "grey", size = .3) + 
+      scale_fill_continuous(name="Age Adjusted Rate", 
+                            low = "blue2", 
+                            high = "brown3", 
+                            na.value = "grey50") + labs(title="Childhood CNS Cancer rates")
+  }, res = 96, height = 550, width = 950)
+}
+
+shinyApp(ui_2, server_2)
