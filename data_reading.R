389a1b87e194ed8fb7f10fd54b525f45ce1b3c96
kaylajeeyoung <kaychoij@bu.edu>
2021-11-22 20:23:11 -0500

    grouping

--- data_reading.R
+++ data_reading.R
@@ -30,6 +30,10 @@ child_death <- read_delim("leading_child_death.txt",
                             delim = "\t", escape_double = FALSE, 
                             trim_ws = TRUE)
 
+child_death2 <- read_delim("leading_child_death2.txt", 
+                           delim = "\t", escape_double = FALSE, 
+                           trim_ws = TRUE)
+
 
 leading_death <- read_delim("leading_death.txt", 
                             delim = "\t", escape_double = FALSE, 
@@ -42,10 +46,14 @@ child_cancer <- clean_text(child_cancer)
 leading_cancer <- clean_text(leading_cancer)
 nl_death <- clean_text(nl_death)
 child_death <- clean_text(child_death)
+child_death2 <- clean_text(child_death2)
 leading_death <- clean_text(leading_death)
 
 options(warn=0)
 
 #obtain only CNS cancers
 df <- child_cancer[grepl("III(b) Astrocytomas", child_cancer$`Childhood Cancers`,fixed = TRUE),]
-
+child_brain_cancer  <- child_cancer[grepl((child_cancer$`Childhood Cancers`), pattern = "III"),]
+child_brain_cancer <- child_brain_cancer %>% group_by(across(c(States, Year))) %>% summarise(Count = sum(Count), 
+                                                                                             Population = unique(Population),
+                                                                                             Rate = sum(Count)/unique(Population) * 10000) 
