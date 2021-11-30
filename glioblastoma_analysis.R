60b64357f3cd53fbd735502128644ff3c08fafd6
kaylajeeyoung <kaychoij@bu.edu>
2021-11-30 09:35:31 -0500

    fix variables

--- glioblastoma_analysis.R
+++ glioblastoma_analysis.R
@@ -18,14 +18,26 @@ brain_rate <- clean_text(read_delim("brain_rate.txt",
                                      delim = "\t", escape_double = FALSE, 
                                      trim_ws = TRUE))
 
-
+#clean up brain incidence
 age_group <- strsplit(brain_incident$`Age Groups Code`, "-")
-age_group <- sapply(age_test, "[[", 1)
-age_group <- as.numeric(substr(age_test, 1, 2))
+age_group <- sapply(age_group, "[[", 1)
+age_group <- as.numeric(substr(age_group, 1, 2))
 brain_incident <- cbind(brain_incident, age_group)
 
 #visualize the brain incidence rate by age group 
 ggplot(brain_incident, aes(x = age_group, y = Count)) + theme_bw() + 
   theme(axis.text.x=element_text(angle=45,hjust=1)) + geom_jitter()
 
+#clean up brain deaths
+age_group <- strsplit(brain_death$`Age Group Code`, "-")
+age_group <- sapply(age_group, "[[", 1)
+age_group <- as.numeric(substr(age_group, 1, 2))
+brain_death <- cbind(brain_death, age_group)
 
+#visualize the brain incidence rate by age group 
+ggplot(brain_death, aes(x = age_group, y = Deaths)) + theme_bw() + 
+  theme(axis.text.x=element_text(angle=45,hjust=1)) + geom_jitter()
+
+
+#clean up brain rates
+brain_rate$`Mortality-Incidence Age-Adjusted Rate Ratio`
